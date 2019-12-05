8994712e3b2663ea079666ab309bb6aa760e9c2d
Joseorina <orinangare@gmail.com>
2019-12-05 19:47:04 +0300

    ft-run-training

--- packages/regression_model/train_pipeline.py
+++ packages/regression_model/train_pipeline.py
@@ -7,3 +7,27 @@ DATASET_DIR = PACKAGE_ROOT /'datasets'
 TESTING_DATA_FILE = DATASET_DIR / 'test.csv'
 TRAINING_DATA_FILE = DATASET_DIR / 'train.csv'
 TARGET = 'SalePrice'
+
+FEATURES = ['MSSubClass', 'MSZoning', 'Neighborhood', 'OverallQual',
+            'OverallCond', 'YearRemodAdd', 'RoofStyle', 'MasVnrType',
+            'BsmtQual', 'BsmtExposure', 'HeatingQC', 'CentralAir',
+            '1stFlrSF', 'GrLivArea', 'BsmtFullBath', 'KitchenQual',
+            'Fireplaces', 'FireplaceQu', 'GarageType', 'GarageFinish',
+            'GarageCars', 'PavedDrive', 'LotFrontage',
+            # this variable is only to calculate temporal variable:
+            'YrSold']
+
+def save_pipeline() -> None:
+    """
+    Persist the pipeline
+    """
+    pass
+
+def run_training() -> None:
+    """
+    train the model
+    """
+    print('Training...')
+
+if __name__ == "__main__":
+    run_training()
