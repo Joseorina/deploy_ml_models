4244a311d9cd20b8489dfcc2f41e6da2bde8d2c6
Joseorina <orinangare@gmail.com>
2019-12-09 14:23:02 +0300

    pipeline

--- packages/regression_model/pipeline.py
+++ packages/regression_model/pipeline.py
@@ -1,2 +1,9 @@
 from sklearn.linear_model imort Lasso
 from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import MinMaxScaler
+
+import preprocessors as pp
+
+CATEGORICAL_VARS_WITH_NA = [
+    ''
+]
